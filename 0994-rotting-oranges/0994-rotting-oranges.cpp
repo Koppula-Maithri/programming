7fd582e7e4bc98767989e668c58d12a214c6b417
Koppula Maithri <166385086+Koppula-Maithri@users.noreply.github.com>
2025-06-20 18:40:32 +0530

    Time: 0 ms (100%), Space: 17 MB (55.95%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.cpp
+++ 0994-rotting-oranges/0994-rotting-oranges.cpp
@@ -1,56 +1,51 @@
 class Solution {
 public:
     int orangesRotting(vector<vector<int>>& grid) {
-        int n = grid.size();
-        int m = grid[0].size();
-   vector<vector<int>> vis(n, vector<int>(m, 0));
-        queue<pair<pair<int,int>,int>> q;
-        int cntf = 0;
-
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j < m; j++){
-                if(grid[i][j] == 2){
+        int n=grid.size();
+         int m=grid[0].size();
+         vector<vector<int>>vis(n,vector<int>(m,0));
+         queue<pair<pair<int,int>,int>>q;
+         int cnt=0;
+         for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(grid[i][j]==2){
+                    vis[i][j]=2;
                     q.push({{i,j},0});
-                    vis[i][j] = 2;
                 }
                 else{
-                    vis[i][j] = 0;
+                    vis[i][j]=0;
                 }
-                if(grid[i][j] == 1){
-                    cntf++;
-                }
-            }
-        }
-
-        int row[] = {-1, 0, 1, 0};
-        int col[] = {0, 1, 0, -1};
-        int tmax = 0;
-        int cnt = 0;
-
-        while(!q.empty()){
-            int r = q.front().first.first;
-            int c = q.front().first.second;
-            int t = q.front().second;
-            q.pop();
-
-            tmax = max(t, tmax);
-
-            for(int i = 0; i < 4; i++){
-                int ro = r + row[i];
-                int co = c + col[i];
-                if(ro >= 0 && ro < n && co >= 0 && co < m && vis[ro][co] == 0 && grid[ro][co] == 1){
-                    q.push({{ro, co}, t + 1});
-                    vis[ro][co] = 2;
+                if(grid[i][j]==1){
                     cnt++;
                 }
             }
-        }
-
-        if(cnt != cntf){
+         }
+         int ro[]={-1,0,1,0};
+         int co[]={0,1,0,-1};
+         int cntf=0;
+         int maxt=0;
+         while(!q.empty()){
+            int r=q.front().first.first;
+            int c=q.front().first.second;
+            int t=q.front().second;
+            q.pop();
+             maxt=max(t,maxt);
+            for(int i=0;i<4;i++){
+                int nr=r+ro[i];
+                int nc=c+co[i];
+                if(nr>=0&&nc>=0&&nr<n&&nc<m&&grid[nr][nc]==1&&!vis[nr][nc]){
+                    cntf++;
+                    vis[nr][nc]=2;
+                    q.push({{nr,nc},t+1});
+                }
+            }
+         }
+         if(cntf==cnt){
+            return maxt;
+         }
+         else{
             return -1;
-        }
-        else{
-            return tmax;
-        }
+         }
+
     }
 };
